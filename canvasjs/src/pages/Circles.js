d38ce9dbfb06e95edb2896d522d4c5bc01aa0dca
EdenWolf <edenwolfson93@gmail.com>
2021-07-31 14:02:32 +0300

    fixed canvas size

--- canvasjs/src/pages/Circles.js
+++ canvasjs/src/pages/Circles.js
@@ -4,7 +4,7 @@ function useWindowSize() {
   const [size, setSize] = useState([0, 0]);
   useLayoutEffect(() => {
     function updateSize() {
-      setSize([window.innerWidth, window.innerHeight - 4]);
+      setSize([window.innerWidth, window.innerHeight]);
     }
     window.addEventListener('resize', updateSize);
     updateSize();
