c92b96392a8e86f524ae20095dbe0db026fc39ec
EdenWolf <edenwolfson93@gmail.com>
2021-08-02 19:47:22 +0300

    1

--- canvasjs/src/pages/Pong.js
+++ canvasjs/src/pages/Pong.js
@@ -13,25 +13,23 @@ export default function Pong(props) {
     this.y = y;
     this.dy = dy;
     this.dx = dx;
-    this.radius = radius;
+    this.radius = height / 50;
 
     this.draw = function () {
       ctx.beginPath();
       ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-      ctx.fillStyle = this.color;
+      ctx.fillStyle = "white";
       ctx.fill();
     };
 
     this.update = function () {
-      if (this.x <= this.radius || this.x >= width - this.radius) {
+      if (this.x <= this.radius || this.x + this.radius >= width) {
         this.dx *= -1;
       }
-      if (this.y + this.radius + this.dy >= height) {
-        this.dy = -this.dy * 0.9;
-        this.dx *= 0.9;
-      } else {
-        this.dy++;
+      if (this.y <= this.radius || this.y + this.radius >= height) {
+        this.dy *= -1;
       }
+
       this.y += this.dy;
       this.x += this.dx;
 
@@ -39,11 +37,48 @@ export default function Pong(props) {
     };
   }
 
+  function Player(x, y, dy) {
+    this.x = x;
+    this.y = y;
+    this.dy = dy;
+    this.width = width / 100;
+    this.height = height / 8;
+
+    this.draw = function () {
+      ctx.fillStyle = "white";
+      ctx.fillRect(this.x, this.y, this.width, this.height);
+    };
+
+    this.update = function () {
+      // if (this.x <= this.radius || this.x >= width - this.radius) {
+      //   this.dx *= -1;
+      // }
+      // if (this.y + this.radius + this.dy >= height) {
+      //   this.dy = -this.dy * 0.9;
+      //   this.dx *= 0.9;
+      // } else {
+      //   this.dy++;
+      // }
+      // this.y += this.dy;
+      // this.x += this.dx;
+
+      this.draw();
+    };
+  }
+
+  let ball = new Ball(width / 2, height / 2, 1, 1, 10);
+  let player1 = new Player(50, height / 2, 0);
+  let player2 = new Player(width - 50, height / 2, 0);
+
   function animate() {
     requestAnimationFrame(animate);
     ctx.clearRect(0, 0, width, height);
     ctx.fillStyle = "#00171a";
     ctx.fillRect(0, 0, width, height);
+
+    ball.update();
+    player1.update();
+    player2.update();
   }
 
   useEffect(() => {
