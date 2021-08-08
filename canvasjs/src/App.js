73d055a8b73aa5142479ab7149a5af3e2b476282
EdenWolf <edenwolfson93@gmail.com>
2021-08-08 17:32:18 +0300

    1

--- canvasjs/src/App.js
+++ canvasjs/src/App.js
@@ -1,44 +1,78 @@
-import './App.css';
-import Circles from './pages/Circles';
-import Gravity from './pages/Gravity';
-import Pong from './pages/Pong';
+import "./App.css";
+import Circles from "./pages/Circles";
+import Gravity from "./pages/Gravity";
+import Pong from "./pages/Pong";
 import { Route, Switch, Link, useLocation } from "react-router-dom";
-import { useState, useLayoutEffect } from 'react';
-
+import { useState, useLayoutEffect } from "react";
 
 function App() {
   useLocation();
-  
+
   function useWindowSize() {
     const [size, setSize] = useState([0, 0]);
     useLayoutEffect(() => {
       function updateSize() {
         setSize([window.innerWidth, window.innerHeight]);
       }
-      window.addEventListener('resize', updateSize);
+      window.addEventListener("resize", updateSize);
       updateSize();
-      return () => window.removeEventListener('resize', updateSize);
+      return () => window.removeEventListener("resize", updateSize);
     }, []);
     return size;
   }
 
+  function randomIntFromRange(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+
+  function getDistance(x1, y1, x2, y2) {
+    let xDistance = x2 - x1;
+    let yDistance = y2 - y1;
+
+    return Math.sqrt(Math.pow(xDistance, 2) + Math.pow(yDistance, 2));
+  }
+
   return (
     <div className="App">
-        <div className="container">
-          <nav>
-            <ul>
-              <li><Link to="/Circles">Circles</Link></li>
-              <li><Link to="/Gravity">Gravity</Link></li>
-              <li><Link to="/Pong">Pong</Link></li>
-            </ul>
-          </nav>
-        </div>
-        <Switch>
-          <Route path="/Circles" component={() => <Circles useWindowSize={useWindowSize} />} />
-          <Route path="/Gravity" component={() => <Gravity useWindowSize={useWindowSize} />} />
-          <Route path="/Pong" component={() => <Pong useWindowSize={useWindowSize}/>} />
-          <Route path="/" component={() => <Circles useWindowSize={useWindowSize} />} />
-        </Switch>
+      <div className="container">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/Circles">Circles</Link>
+            </li>
+            <li>
+              <Link to="/Gravity">Gravity</Link>
+            </li>
+            <li>
+              <Link to="/Pong">Pong</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <Switch>
+        <Route
+          path="/Circles"
+          component={() => <Circles useWindowSize={useWindowSize} />}
+        />
+        <Route
+          path="/Gravity"
+          component={() => <Gravity useWindowSize={useWindowSize} />}
+        />
+        <Route
+          path="/Pong"
+          component={() => (
+            <Pong
+              useWindowSize={useWindowSize}
+              randomIntFromRange={randomIntFromRange}
+              getDistance={getDistance}
+            />
+          )}
+        />
+        <Route
+          path="/"
+          component={() => <Circles useWindowSize={useWindowSize} />}
+        />
+      </Switch>
     </div>
   );
 }
--- canvasjs/src/pages/Pong.js
+++ canvasjs/src/pages/Pong.js
@@ -2,6 +2,8 @@ import { useEffect } from "react";
 
 export default function Pong(props) {
   let useWindowSize = props.useWindowSize;
+  let randomIntFromRange = props.randomIntFromRange;
+  let getDistance = props.getDistance;
 
   const [width, height] = useWindowSize();
 
@@ -11,8 +13,9 @@ export default function Pong(props) {
   function Ball(x, y, dx, dy, radius) {
     this.x = x;
     this.y = y;
-    this.dy = dy;
-    this.dx = dx;
+    this.speed = 10;
+    this.dy = dy * this.speed;
+    this.dx = dx * this.speed;
     this.radius = height / 50;
 
     this.draw = function () {
@@ -28,6 +31,39 @@ export default function Pong(props) {
       }
       if (this.y <= this.radius || this.y + this.radius >= height) {
         this.dy *= -1;
+      }
+
+      // Left Player
+      let inLeftPlayerXRange =
+        playerL.x + playerL.width + this.radius >= this.x;
+      let inLeftPlayerYRange =
+        playerL.y <= this.y && playerL.y + playerL.height >= this.y;
+      let closeToLeftPlayerCorner =
+        getDistance(this.x, this.y, playerL.x + playerL.width, playerL.y) <=
+          this.radius &&
+        getDistance(
+          this.x,
+          this.y,
+          playerL.x + playerL.width,
+          playerL.y + playerL.height
+        ) <= this.radius;
+      let collidedLeftPlayer =
+        closeToLeftPlayerCorner || (inLeftPlayerXRange && inLeftPlayerYRange);
+
+      // Right Player
+      let inRightPlayerXRange = playerR.x - this.radius <= this.x;
+      let inRightPlayerYRange =
+        playerR.y <= this.y && playerR.y + playerR.height >= this.y;
+      let closeToRightPlayerCorner =
+        getDistance(this.x, this.y, playerR.x, playerR.y) <= this.radius &&
+        getDistance(this.x, this.y, playerR.x, playerR.y + playerR.height) <=
+          this.radius;
+      let collidedRightPlayer =
+        closeToRightPlayerCorner ||
+        (inRightPlayerXRange && inRightPlayerYRange);
+
+      if (collidedLeftPlayer || collidedRightPlayer) {
+        this.dx *= -1;
       }
 
       this.y += this.dy;
@@ -50,25 +86,13 @@ export default function Pong(props) {
     };
 
     this.update = function () {
-      // if (this.x <= this.radius || this.x >= width - this.radius) {
-      //   this.dx *= -1;
-      // }
-      // if (this.y + this.radius + this.dy >= height) {
-      //   this.dy = -this.dy * 0.9;
-      //   this.dx *= 0.9;
-      // } else {
-      //   this.dy++;
-      // }
-      // this.y += this.dy;
-      // this.x += this.dx;
-
       this.draw();
     };
   }
 
   let ball = new Ball(width / 2, height / 2, 1, 1, 10);
-  let player1 = new Player(50, height / 2, 0);
-  let player2 = new Player(width - 50, height / 2, 0);
+  let playerL = new Player(50, height / 2, 0);
+  let playerR = new Player(width - 50, height / 2, 0);
 
   function animate() {
     requestAnimationFrame(animate);
@@ -76,9 +100,9 @@ export default function Pong(props) {
     ctx.fillStyle = "#00171a";
     ctx.fillRect(0, 0, width, height);
 
+    playerL.update();
+    playerR.update();
     ball.update();
-    player1.update();
-    player2.update();
   }
 
   useEffect(() => {
